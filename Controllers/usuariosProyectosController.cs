ff84975d1d961785da76a66c5500bbb9aedde3e4
Lourdes Aguirre <loumaagui@gmail.com>
2021-08-05 12:48:02 -0600

    vista desarrolladores y metodo post añadido

---
+++ Controllers/usuariosProyectosController.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSP_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuariosProyectosController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.UsuarioProyecto modelo)
+        {
+            using (Models.DBPSPPLUSContext db = new Models.DBPSPPLUSContext())
+            {
+                Models.UsuarioProyecto usuarioP = new Models.UsuarioProyecto();
+
+                usuarioP.IdProyecto = modelo.IdProyecto;
+                usuarioP.IdUsuario = modelo.IdUsuario;
+
+                db.UsuarioProyectos.Add(usuarioP);
+                db.SaveChanges();
+
+            }
+
+            return Ok("El usuario se añadio correctamente a un proyecto");
+        }
+    }
+}
