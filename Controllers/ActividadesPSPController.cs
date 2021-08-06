2a3ba4a63a93c7c48cd1c29f4f58b70803104faa
Erick Echeverría <erickecheverria66@gmail.com>
2021-08-06 00:26:55 -0600

    CRUD Tiempos PSP Terminado + validaciones realizadas

---
+++ Controllers/ActividadesPSPController.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// DOCUMENTO RELIZADO POR: Erick Eduardo Echeverría Garrido (EE) 5/08/2021 
+
+namespace PSP_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActividadesPSPController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get(int idUsuario, int? idProyecto, int? buscarProyecto, int? idTiempoPSP, int? actividadesSinProyecto)
+        {
+            using (Models.DBPSPPLUSContext db = new Models.DBPSPPLUSContext())
+            {
+                if(idTiempoPSP != null)
+                {
+                    var actividades = (from d in db.TiemposPsps
+                                       select d).Where(d => d.IdTiempoPsp == idTiempoPSP).Where(d => d.IdUsuario == idUsuario).ToList();
+
+                    return Ok(actividades);
+                }
+
+                if (actividadesSinProyecto != null)
+                {
+                    var actividades = (from d in db.TiemposPsps
+                                       select d).Where(d => d.IdProyecto == null).ToList();
+
+                    return Ok(actividades);
+                }
+
+                if (idProyecto == null && buscarProyecto == null)
+                {
+                    var actividades = (from d in db.TiemposPsps
+                                            select d).Where(d => d.IdUsuario == idUsuario).OrderBy(d => d.FechaHoraInicio).ToList();
+
+                    return Ok(actividades);
+                }
+                else
+                {
+                    if (buscarProyecto == null)
+                    {
+                        var actividades = (from d in db.TiemposPsps
+                                           select d).Where(d => d.IdUsuario == idUsuario).Where(d => d.IdProyecto == idProyecto).OrderBy(d => d.FechaHoraInicio).ToList();
+
+                        return Ok(actividades);
+                    }
+                    else
+                    {
+
+                        var proyectos = (from p in db.Proyectos join d in db.UsuarioProyectos on p.IdProyecto equals d.IdProyecto where d.IdUsuario == idUsuario select p).ToList();
+
+
+                        return Ok(proyectos);
+                    }
+
+                        
+                }
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult Post(DateTime fechaHoraInicio, DateTime fechaHoraFinal, string descripcion, int? idProyecto, int idUsuario)
+        {
+            using (Models.DBPSPPLUSContext db = new Models.DBPSPPLUSContext())
+            {
+                Models.TiemposPsp actividad = new Models.TiemposPsp();
+
+                actividad.FechaHoraInicio = fechaHoraInicio;
+                actividad.FechaHoraFinal = fechaHoraFinal;
+                actividad.Descripcion = descripcion;
+                if(idProyecto != null)
+                {
+                    actividad.IdProyecto = idProyecto;
+                }
+                actividad.IdUsuario = idUsuario;
+ 
+                db.TiemposPsps.Add(actividad);
+                db.SaveChanges();
+
+            }
+
+            return Ok("La actividad se agrego correctamente");
+        }
+
+
+        [HttpDelete]
+        public ActionResult Delete(int idTiempoPSP)
+        {
+            using (Models.DBPSPPLUSContext db = new Models.DBPSPPLUSContext())
+            {
+                try
+                {
+                   Models.TiemposPsp actividad = db.TiemposPsps.Find(idTiempoPSP);
+
+                    db.TiemposPsps.Remove(actividad);
+                    db.SaveChanges();
+
+                    return Ok("La actividad se elimino correctamente");
+                }
+                catch (Exception)
+                {
+                    return Ok("Error");
+                }
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put(int idTiempoPSP, DateTime fechaHoraInicio, DateTime fechaHoraFinal, string descripcion, int? idProyecto)
+        {
+            using (Models.DBPSPPLUSContext db = new Models.DBPSPPLUSContext())
+            {
+                Models.TiemposPsp datos = db.TiemposPsps.Find(idTiempoPSP);
+
+                datos.FechaHoraInicio = fechaHoraInicio;
+                datos.FechaHoraFinal = fechaHoraFinal;
+                datos.Descripcion = descripcion;
+                if(idProyecto != null)
+                {
+                    datos.IdProyecto = idProyecto;
+                }
+                else
+                {
+                    datos.IdProyecto = null;
+                }
+
+                db.Entry(datos).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                db.SaveChanges();
+
+                return Ok("Datos Actualizados Correctamente");
+
+            }
+        }
+
+
+    }
+}
---
+++ Views/ActividadesPSP/Agregar-Actividad/js/agregarActividad.js
@@ -0,0 +1,126 @@
+// DOCUMENTO RELIZADO POR: Erick Eduardo Echeverría Garrido (EE)
+
+// ---------------------------------- Funciones cookies ----------------------------------
+/*function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+
+const stringJWT = Cookies.get('jwt');
+let jwt;
+
+if (stringJWT) {
+    jwt = parseJwt(stringJWT);
+}
+
+function CerrarSesion() {
+    Cookies.remove('jwt');
+};
+
+const idUsuario = jwt.sub;*/
+// ---------------------------------- FIN Funciones cookies ----------------------------------
+
+let url = 'https://localhost:44368';
+const idUsuario = 1;
+
+// SELECTORES
+const alerta = document.querySelector('#alert');
+const alertaFechasIncorrectas = document.querySelector('#alert2');
+
+const formulario = document.querySelector('#formulario');
+const descripcionInput = document.querySelector('#descripcion');
+const fechaHoraInicioInput = document.querySelector('#fechaHoraInicio');
+const fechaHoraFinalInput = document.querySelector('#fechaHoraFinal');
+const proyectosSelect = document.querySelector('#proyectos-listado');
+
+
+window.onload = () => {
+    obtenerProyectos();
+}
+
+async function obtenerProyectos(){
+    const direccion = `${url}/api/ActividadesPSP?idUsuario=${idUsuario}&buscarProyecto=1`;
+
+    await fetch(direccion)
+        .then(respuesta => respuesta.json())
+        .then( resultado => resultado)
+        .then( proyectos => selectProyecto(proyectos))
+}
+
+function selectProyecto(proyectos) {
+    proyectos.forEach( proyecto => {
+       const { nombre, idProyecto } = proyecto;
+
+       const option = document.createElement('option');
+       option.value = idProyecto;
+       option.textContent = nombre;
+       proyectosSelect.appendChild(option);
+
+    })
+}
+
+function validarDatos(){
+    if(descripcionInput.value == '' || fechaHoraInicioInput.value == '' || fechaHoraFinalInput.value == '' || proyectosSelect.value == ''){
+        
+        alerta.style.display = 'block';
+
+        setTimeout(() => {
+            alerta.style.display = 'none';
+        }, 3000);
+
+        return;
+    }
+
+    if(fechaHoraInicioInput.value > fechaHoraFinalInput.value){
+        alertaFechasIncorrectas.style.display = 'block';
+
+        setTimeout(() => {
+            alertaFechasIncorrectas.style.display = 'none';
+        }, 3000);
+
+        return;
+    }
+
+    agregarActividad();
+}
+
+async function agregarActividad(){
+
+    mostrarSpinner();
+
+    let direccion;
+
+    if(proyectosSelect.value == '0'){
+        direccion = `${url}/api/ActividadesPSP?fechaHoraInicio=${fechaHoraInicioInput.value}&fechaHoraFinal=${fechaHoraFinalInput.value}&descripcion=${descripcionInput.value}&idUsuario=${idUsuario}`;
+    }else{
+        direccion = `${url}/api/ActividadesPSP?fechaHoraInicio=${fechaHoraInicioInput.value}&fechaHoraFinal=${fechaHoraFinalInput.value}&descripcion=${descripcionInput.value}&idProyecto=${proyectosSelect.value}&idUsuario=${idUsuario}`;
+    }
+
+    await fetch(direccion,{
+        method: 'POST'})
+        .then(respuesta => respuesta)
+        .then(resultado => {
+        })
+
+    alert('Agregado Exitosamente');
+    window.location.href = (`../MenuActividades.html`);
+}
+
+function mostrarSpinner(){
+
+    const spinner = document.createElement('div');
+    spinner.classList.add('spinner');
+
+    spinner.innerHTML = `
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    `;
+
+    formulario.appendChild(spinner);
+}
