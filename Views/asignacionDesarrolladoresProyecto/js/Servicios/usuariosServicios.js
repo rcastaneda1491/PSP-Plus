e84c01ec27dccbba1343d4f275b7a0b38d96543e
Lourdes Aguirre <loumaagui@gmail.com>
2021-08-23 17:32:49 -0600

    header agregados

--- Views/asignacionDesarrolladoresProyecto/js/Servicios/usuariosServicios.js
+++ Views/asignacionDesarrolladoresProyecto/js/Servicios/usuariosServicios.js
@@ -1,4 +1,9 @@
 const requestUrl = `${URL}/ComboDesarrolladores`
+const headers = {
+  'Accept' : "application/json",
+  "Content-Type": "application/json",
+  'Authorization': 'Bearer ' + stringJWT
+};
 
 
 const usuarioService = {
